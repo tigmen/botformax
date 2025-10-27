02d99993f59e989726852c2766ead0db1d6a6610
tigmen <tigmenal@gmail.com>
2025-10-27 08:41:32 +0000

    Работающий зеркальный бот Принимает сообщение с текстом A Отправляет в ответ тот же текст

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: index
 
-index: _client.o _trace.o _net.o
-	gcc _trace.o _net.o _client.o -lcurl -ljson-c -o index
+index: _client.o _trace.o _net.o _botapi.o
+	gcc _trace.o _net.o _botapi.o _client.o -lcurl -ljson-c -o index
 
 _client.o: client.c
 	gcc -c -g client.c -o _client.o
@@ -11,6 +11,9 @@ _trace.o: trace/trace.c trace/trace.h
 
 _net.o: net.h net.c
 	gcc -c net.c -o _net.o
+
+_botapi.o: botapi.c botapi.h
+	gcc -c botapi.c -o _botapi.o
 
 clean:
 	rm -rf *.o vgcore.* log index
--- client.c
+++ client.c
@@ -3,6 +3,7 @@
 #include <json-c/json.h>
 #include <stdlib.h>
 #include <string.h>
+#include "botapi.h"
 #include "net.h"
 #include "trace/trace.h"
 
@@ -10,110 +11,27 @@
 	"https://api.telegram.org/" \
 	"bot7515996637:AAGz4Me9uTw2K-vXwK5SvD7oQ4iU_ZtG18w"
 
-#define sym(x) #x
+#define SENDMESSAGE "/sendMessage"
+void send_message(char* url, Chat* chat, char* text) {
+	char* temp = malloc(strlen(url) + strlen(SENDMESSAGE) + 1);
+	temp = strcpy(temp, url);
+	temp = strcat(temp, SENDMESSAGE);
+	trace(DEBUG, "GET request to %s", temp);
 
-typedef int64_t bigint;
-typedef bigint user_id;
-typedef bigint chat_id;
-typedef bigint Date;
+	struct json_object* param = json_object_new_object();
+	if (!param) {
+		trace(ERROR, "%s %d: json not created", __FILE__, __LINE__);
+		exit(0);
+	}
 
-typedef struct {
-	user_id user_id;
-	char is_bot;
-	char* first_name;
-	char* username;
-} User;
+	struct json_object* json;
+	json_object_object_add(param, "chat_id",
+	                       json_object_new_int(chat->chat_id));
+	json_object_object_add(param, "text", json_object_new_string(text));
+	json = request_get(temp, json_object_to_json_string(param));
+	json_object_put(param);
 
-typedef struct {
-	chat_id chat_id;
-	char* first_name;
-	char* username;
-	char* type;
-} Chat;
-
-typedef struct {
-	bigint message_id;
-	User* from;
-	Chat* chat;
-	Date date;
-	char* text;
-} Message;
-
-typedef struct {
-	bigint update_id;
-	Message* message;
-} Update;
-
-struct update_node {
-	Update* data;
-	struct update_node* next;
-};
-
-struct update_queue {
-	struct update_node* head;
-	struct update_node* tail;
-};
-
-void update_queue_init(struct update_queue**);
-struct update_queue* update_queue_delete(struct update_queue*);
-void update_queue_push(struct update_queue*, Update*);
-Update* update_queue_pop(struct update_queue*);
-char update_queue_is_empty(struct update_queue*);
-
-void copy_str(char** to, const char* from) {
-	*to = malloc(strlen(from)) + 1;
-	*to = strcpy(*to, from);
-}
-
-#define json_get(type, root, key) \
-	(json_object_get_##type(json_object_object_get(root, key)))
-Chat* chat_parse(struct json_object* json) {
-	Chat* chat = malloc(sizeof(Chat));
-	chat->chat_id = json_get(int, json, "chat_id");
-	chat->first_name = (char*)json_get(string, json, "first_name");
-	chat->username = (char*)json_get(string, json, "username");
-	chat->type = (char*)json_get(string, json, "type");
-	return chat;
-}
-
-User* user_parse(struct json_object* json) {
-	User* user = malloc(sizeof(User));
-	user->user_id = json_get(int, json, "user_id");
-	user->first_name = (char*)json_get(string, json, "first_name");
-	user->username = (char*)json_get(string, json, "username");
-	return user;
-}
-
-Message* message_parse(struct json_object* json, int* offset) {
-	Message* message = malloc(sizeof(Message));
-	message->message_id = json_get(int, json, "message_id");
-
-	struct json_object* json_user = json_object_object_get(json, "from");
-
-	User* user = user_parse(json_user);
-	message->from = user;
-
-	struct json_object* json_chat = json_object_object_get(json, "chat");
-
-	Chat* chat = chat_parse(json_chat);
-	message->chat = chat;
-
-	message->date = json_get(int, json, "date");
-	message->text = (char*)json_get(string, json, "text");
-
-	return message;
-}
-Update* update_parse(struct json_object* json) {
-	Update* update = malloc(sizeof(Update));
-	update->update_id = json_get(int, json, "update_id");
-
-	struct json_object* json_message =
-	    json_object_object_get(json, "message");
-
-	Message* message = message_parse(json_message, (int*)0);
-	update->message = message;
-
-	return update;
+	free(temp);
 }
 
 #define GETUPDATES "/getUpdates"
@@ -156,6 +74,9 @@ struct update_queue* get_updates(char* url) {
 		      update_queue_is_empty(result) ? "Y" : "n");
 
 		Update* update = update_parse(json_temp);
+
+		send_message(url, update->message->chat, update->message->text);
+
 		offset = update->update_id + 1;
 		update_queue_push(result, update);
 	}
@@ -184,55 +105,4 @@ int main(void) {
 	}
 
 	curl_global_cleanup();
-}
-
-void update_queue_init(struct update_queue** queue) {
-	*queue = malloc(sizeof(struct update_queue));
-	(*queue)->tail = NULL;
-	(*queue)->head = NULL;
-}
-
-struct update_queue* update_queue_delete(struct update_queue* queue) {
-	if (queue != NULL) {
-		while (!update_queue_is_empty(queue)) {
-			update_queue_pop(queue);
-		}
-	}
-	return NULL;
-}
-
-void update_queue_push(struct update_queue* queue, Update* data) {
-	struct update_node* new = malloc(sizeof(struct update_node));
-	new->next = NULL;
-	new->data = data;
-
-	if (update_queue_is_empty(queue)) {
-		queue->tail = new;
-		queue->head = new;
-	} else {
-		queue->tail->next = new;
-		queue->tail = new;
-	}
-}
-
-Update* update_queue_pop(struct update_queue* queue) {
-	Update* data;
-	if (!update_queue_is_empty(queue)) {
-		struct update_node* node = queue->head;
-		queue->head = node->next;
-
-		data = node->data;
-
-		free(node);
-
-	} else {
-		trace(WARNING, "%s %d: try to pop empty queue", __FILE__,
-		      __LINE__);
-		data = NULL;
-	}
-	return data;
-}
-
-inline char update_queue_is_empty(struct update_queue* queue) {
-	return queue->head == NULL;
 }
