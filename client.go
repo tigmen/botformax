f307b196fc6508d5beb14555ecf87e03a2d988ba
tigmen <tigmenal@gmail.com>
2025-10-31 23:01:03 +0000

    Запрос публичного ip GetHost() Интерфейсы для работы с http Reader

--- client.go
+++ client.go
@@ -1,5 +1,65 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type Reader interface {
+	Read(*http.Response)
+}
+
+type ReaderToFile struct {
+	filepath string
+}
+
+func (reader ReaderToFile) Read(resp *http.Response) {
+	out, err := os.Create(reader.filepath)
+	if err != nil {
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+	}
+}
+
+type ReaderToString struct {
+	str *string
+}
+
+func (reader ReaderToString) Read(resp *http.Response) {
+	out, err := io.ReadAll(resp.Body)
+	if err != nil {
+	}
+	*(reader.str) = string(out)
+}
+
+const url = "https://ipv4.icanhazip.com"
+func GetHost() string{
+	var out string
+
+	resp, err := http.Get(url)
+	if err != nil {
+	}
+	defer resp.Body.Close()
+
+	var reader Reader = ReaderToString{&out}
+	reader.Read(resp)
+
+	return out
+}
+
 func main() {
-	
+	// url := "https://api.telegram.org/bot7515996637:AAGz4Me9uTw2K-vXwK5SvD7oQ4iU_ZtG18w/getMe"
+	url := "https://ipv4.icanhazip.com"
+	// filepath := "out"
+	resp, err := http.Get(url)
+	if err != nil {
+	}
+	defer resp.Body.Close()
+
+	fmt.Printf("%s\n", GetHost())
 }
