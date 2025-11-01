c8154fc8446c60be5a5916aa9427dc7300a5569a
tigmen <tigmenal@gmail.com>
2025-11-01 12:01:33 +0000

    added print

--- src/client.go
+++ src/client.go
@@ -74,4 +74,5 @@ func main() {
 func HelloServer(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte("This is an example server.\n"))
+	fmt.Fprintf(w, "This is an example server.\n")
 }
