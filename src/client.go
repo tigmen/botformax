61971bdaaf7a0e6def2a6ed1d85a92ba3ea7747a
tigmen <tigmenal@gmail.com>
2025-11-06 01:02:17 +0300

    Загрузка аудиофалов из чатов(в том числе групповых)

--- src/client.go
+++ src/client.go
@@ -1,24 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 	"os"
-
-	"golang.org/x/crypto/acme/autocert"
 )
 
 type Reader interface {
 	Read(*http.Response)
 }
 
-type ReaderToFile struct {
+type ReaderFile struct {
 	filepath string
 }
 
-func (reader ReaderToFile) Read(resp *http.Response) {
+func (reader ReaderFile) Read(resp *http.Response) {
 	out, err := os.Create(reader.filepath)
 	if err != nil {
 	}
@@ -29,78 +27,151 @@ func (reader ReaderToFile) Read(resp *http.Response) {
 	}
 }
 
-type ReaderToString struct {
+type ReaderString struct {
 	str *string
 }
 
-func (reader ReaderToString) Read(resp *http.Response) {
+func (reader ReaderString) Read(resp *http.Response) {
 	out, err := io.ReadAll(resp.Body)
 	if err != nil {
 	}
+
 	if out == nil {
 		*(reader.str) = "nil"
 	}
 	*(reader.str) = string(out)
 }
 
-const GETHOSTURL = "https://ipv4.icanhazip.com"
+type ReaderJson struct {
+	out *map[string]any
+}
 
-func GetHost() string {
-	var out string
-
-	resp, err := http.Get(GETHOSTURL)
+func (reader ReaderJson) Read(resp *http.Response) {
+	decoder := json.NewDecoder(resp.Body)
+	err := decoder.Decode(reader.out)
 	if err != nil {
+		log.Printf("%#v", err)
+	}
+	defer resp.Body.Close()
+}
+
+const GETUPDATES = "https://platform-api.max.ru/updates"
+
+const (
+	Audio int = 0
+	Video
+)
+
+type Attachment struct {
+	_type int
+	url   string
+	token string
+}
+
+func ParseAttachment(attachment map[string]any) Attachment {
+	new_attachment := new(Attachment)
+
+	switch attachment["type"].(string) {
+	case "audio":
+		new_attachment._type = 1
+
+	case "video":
+		new_attachment._type = 2
+
+	default:
+		new_attachment._type = 0
+	}
+
+	if new_attachment._type > 0 {
+		tmp := attachment["payload"].(map[string]any)
+		new_attachment.url = tmp["url"].(string)
+		new_attachment.token = tmp["token"].(string)
+	}
+
+	return *new_attachment
+}
+
+type Body struct {
+	mid         string
+	text        string
+	attachments []Attachment
+}
+
+func ParseBody(body map[string]any) Body {
+	new_body := new(Body)
+
+	new_body.mid = body["mid"].(string)
+
+	new_body.text = body["text"].(string)
+
+	val, ok := body["attachments"]
+	if ok {
+		for _, value := range val.([]any) {
+			new_body.attachments = append(new_body.attachments, ParseAttachment(value.(map[string]any)))
+		}
+	}
+
+	return *new_body
+}
+
+type Message struct {
+	body Body
+}
+
+func ParseMessage(message map[string]any) Message {
+	new_msg := new(Message)
+	new_msg.body = ParseBody(message["body"].(map[string]any))
+
+	return *new_msg
+}
+
+func Handle(token string) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", GETUPDATES, nil)
+	if err != nil {
+		log.Printf("%#v", err)
+	}
+	req.Header.Add("Authorization", token)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Printf("%#v", err)
 	}
 	defer resp.Body.Close()
 
-	var reader Reader = ReaderToString{&out}
+	update := map[string]any{}
+	var reader Reader = ReaderJson{&update}
 	reader.Read(resp)
 
-	return out
+	for _, value := range update["updates"].([]any) {
+		msg := ParseMessage(value.(map[string]any)["message"].(map[string]any))
+		if len(msg.body.attachments) > 0 && msg.body.attachments[0]._type > 0 {
+			GetAudio(token, msg.body.attachments[0].url)
+		}
+		log.Printf("%s:%s\n", msg.body.mid, msg.body.text)
+	}
+}
+
+func GetAudio(token, url string) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Printf("%#v", err)
+	}
+	req.Header.Add("Authorization", token)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Printf("%#v", err)
+	}
+
+	var reader Reader = ReaderFile{"file.ogg"}
+	reader.Read(resp)
+
 }
 
 func main() {
-	// url := "https://api.telegram.org/bot7515996637:AAGz4Me9uTw2K-vXwK5SvD7oQ4iU_ZtG18w/setWebhook"
-	// url := "https://ipv4.icanhazip.com"
-	// filepath := "out"
-	fmt.Printf("%s", GetHost())
-
-	//  strings.NewReader("{\"url\" : \"https://62.109.1.123:443\"}")
-	// resp, err := http.NewRequest("POST", url, jj)
-
-	// if resp.Response != nil {
-	// 	var out string
-	// 	var reader Reader = ReaderToString{&out}
-	// 	reader.Read(resp.Response)
-
-	// 	fmt.Printf("%s\n", out)
-	// }
-
-	// http.HandleFunc("/", HelloServer)
-	// err := http.ListenAndServeTLS(":443", "keys/server.crt", "keys/server.key", nil)
-	// if err != nil {
-	// 	log.Fatal("ListenAndServe: ", err)
-	// }
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		out, err := io.ReadAll(r.Body)
-		if err != nil {
-
-		}
-		fmt.Fprintf(w, "Hello, TLS user! Your config: %s", out)
-	})
-	log.Fatal(http.Serve(autocert.NewListener(""), mux))
-}
-func HelloServer(w http.ResponseWriter, req *http.Request) {
-	defer req.Body.Close()
-
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("This is an example server.\n"))
-	if req.Response != nil {
-		var out string
-		var reader Reader = ReaderToString{&out}
-		reader.Read(req.Response)
-
-		fmt.Printf("%s", out)
-	}
+	Handle(os.Args[1])
 }
