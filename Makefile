9cdfa69bfdd78ccba00b76caa7d6f45b953b8715
tigmen <110287097+tigmen@users.noreply.github.com>
2025-11-05 22:06:34 +0000

    Загрузка (longpooling) аудиофалов из чатов(в том числе групповых) (#4)
    * Загрузка аудиофалов из чатов(в том числе групповых)

--- Makefile
+++ Makefile
@@ -1,20 +1,6 @@
-all: index
+all: src/client.go go.mod
+	go mod tidy
+	go build -o build/index src/client.go
 
-index: _client.o _trace.o _net.o _botapi.o
-	gcc _trace.o _net.o _botapi.o _client.o -lcurl -ljson-c -o index
-
-_client.o: client.c
-	gcc -c -g client.c -o _client.o
-
-_trace.o: trace/trace.c trace/trace.h
-	gcc -c trace/trace.c -o _trace.o
-
-_net.o: net.h net.c
-	gcc -c net.c -o _net.o
-
-_botapi.o: botapi.c botapi.h
-	gcc -c botapi.c -o _botapi.o
-
-clean:
-	rm -rf *.o vgcore.* log index
-
+go.mod:
+	go mod init main
---
+++ src/client.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Reader interface {
+	Read(*http.Response)
+}
+
+type ReaderFile struct {
+	filepath string
+}
+
+func (reader ReaderFile) Read(resp *http.Response) {
+	out, err := os.Create(reader.filepath)
+	if err != nil {
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+	}
+}
+
+type ReaderString struct {
+	str *string
+}
+
+func (reader ReaderString) Read(resp *http.Response) {
+	out, err := io.ReadAll(resp.Body)
+	if err != nil {
+	}
+
+	if out == nil {
+		*(reader.str) = "nil"
+	}
+	*(reader.str) = string(out)
+}
+
+type ReaderJson struct {
+	out *map[string]any
+}
+
+func (reader ReaderJson) Read(resp *http.Response) {
+	decoder := json.NewDecoder(resp.Body)
+	err := decoder.Decode(reader.out)
+	if err != nil {
+		log.Printf("%#v", err)
+	}
+	defer resp.Body.Close()
+}
+
+const GETUPDATES = "https://platform-api.max.ru/updates"
+
+const (
+	Audio int = 0
+	Video
+)
+
+type Attachment struct {
+	_type int
+	url   string
+	token string
+}
+
+func ParseAttachment(attachment map[string]any) Attachment {
+	new_attachment := new(Attachment)
+
+	switch attachment["type"].(string) {
+	case "audio":
+		new_attachment._type = 1
+
+	case "video":
+		new_attachment._type = 2
+
+	default:
+		new_attachment._type = 0
+	}
+
+	if new_attachment._type > 0 {
+		tmp := attachment["payload"].(map[string]any)
+		new_attachment.url = tmp["url"].(string)
+		new_attachment.token = tmp["token"].(string)
+	}
+
+	return *new_attachment
+}
+
+type Body struct {
+	mid         string
+	text        string
+	attachments []Attachment
+}
+
+func ParseBody(body map[string]any) Body {
+	new_body := new(Body)
+
+	new_body.mid = body["mid"].(string)
+
+	new_body.text = body["text"].(string)
+
+	val, ok := body["attachments"]
+	if ok {
+		for _, value := range val.([]any) {
+			new_body.attachments = append(new_body.attachments, ParseAttachment(value.(map[string]any)))
+		}
+	}
+
+	return *new_body
+}
+
+type Message struct {
+	body Body
+}
+
+func ParseMessage(message map[string]any) Message {
+	new_msg := new(Message)
+	new_msg.body = ParseBody(message["body"].(map[string]any))
+
+	return *new_msg
+}
+
+func Handle(token string) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", GETUPDATES, nil)
+	if err != nil {
+		log.Printf("%#v", err)
+	}
+	req.Header.Add("Authorization", token)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Printf("%#v", err)
+	}
+	defer resp.Body.Close()
+
+	update := map[string]any{}
+	var reader Reader = ReaderJson{&update}
+	reader.Read(resp)
+
+	for _, value := range update["updates"].([]any) {
+		msg := ParseMessage(value.(map[string]any)["message"].(map[string]any))
+		if len(msg.body.attachments) > 0 && msg.body.attachments[0]._type > 0 {
+			GetAudio(token, msg.body.attachments[0].url)
+		}
+		log.Printf("%s:%s\n", msg.body.mid, msg.body.text)
+	}
+}
+
+func GetAudio(token, url string) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Printf("%#v", err)
+	}
+	req.Header.Add("Authorization", token)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Printf("%#v", err)
+	}
+
+	var reader Reader = ReaderFile{"file.ogg"}
+	reader.Read(resp)
+
+}
+
+func main() {
+	Handle(os.Args[1])
+}
